fd2520fb46d8a3a9ebd8f77a2ffe2fa1091de76d
Paparapy <HasiniainaRakotomanana.skype@gmail.com>
2024-10-18 23:02:08 +0300

    modifier emplacement du css

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
-import "./Global.css";
+import "./Home.css";
 
 const Home = () => {
   const containerRef = useRef(null);
