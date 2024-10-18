f64ff32d9519e34db272b1f0392460991f75ba64
Paparapy <HasiniainaRakotomanana.skype@gmail.com>
2024-10-18 20:18:11 +0300

    envoyer le site a netifly

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", // Spécifie le répertoire de sortie
+    outDir: 'build', // Spécifie le répertoire de sortie
   },
-  base: "./",
-});
+  base: './', // Assure que les chemins relatifs sont corrects
+})
